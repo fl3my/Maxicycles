e8ff8310702fce07fff67aabf621f3dacb018739
Ross Fleming <rossflem@icloud.com>
2023-04-07 11:02:02 +0100

    Add dynamic categories

---
+++ Maxicycles/ViewComponents/CategoryViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using Maxicycles.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Maxicycles.ViewComponents;
+
+[ViewComponent(Name = "Category")]
+public class CategoryViewComponent : ViewComponent
+{
+    private readonly MaxicyclesDbContext _context;
+
+    public CategoryViewComponent(MaxicyclesDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        var categories = await _context.Category.Include(c => c.SubCategories).ToListAsync();
+        return View(categories);
+    }
+}
